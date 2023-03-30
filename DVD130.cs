a199227d2aeb4a540734d38b2d78376a336008f6
NafNawal04 <nafisanawal@iut-dhaka.edu>
2023-03-30 10:18:35 +0600

    added the dvd class things

--- DVD130.cs
+++ DVD130.cs
@@ -8,6 +8,18 @@ namespace _210042130_lab10
 {
     internal class DVD130 : Item130 <string>
     {
+        public string director { get; set; }
+        public int length { get; set; }
+
+        public DVD130(string id, string title, string author, string category, string director, int length)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.category = category;
+            this.director = director;
+            this.length = length;
+        }
         public override void displayDetails()
         { }
 
