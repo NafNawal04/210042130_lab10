e8d1979eb34d9b389f3a6851119a8a7e4e09dc06
NafNawal04 <nafisanawal@iut-dhaka.edu>
2023-03-30 09:39:45 +0600

    added necessary classes

---
+++ DVD130.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _210042130_lab10
+{
+    internal class DVD130 : Item130
+    {
+    }
+}
---
+++ LibraryExc130.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _210042130_lab10
+{
+    internal class LibraryExc130 : Exception
+    {
+        public LibraryExc130(string message) : base(message) { }
+    }
+}
