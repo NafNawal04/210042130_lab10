08668f96449353462bc3cddb55e86664c9c73d28
NafNawal04 <nafisanawal@iut-dhaka.edu>
2023-03-30 10:01:10 +0600

    made an abstract class item

--- Item130.cs
+++ Item130.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace _210042130_lab10
 {
-    abstract class Item130
+    abstract class Item130 <T>
     {
+        public T id { get; set; }
+        public T title { get; set; }
+        public T author { get; set; }
+        public T category { get; set; }
 
+        public bool isavailable { get; set; }
+
+        public abstract void displayDetails();
+        public abstract void borrowItem();
+        public abstract void returnItem();
     }
 }
